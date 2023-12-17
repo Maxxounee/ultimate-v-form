6c52bf823edbcb0c14e8d11c77e38404afa3ab04
maxxounee <69703785+Maxxounee@users.noreply.github.com>
2023-12-17 15:26:24 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+import FormWrapper from './components/FormWrapper.vue';
+import FormInput from './components/FormInput.vue';
+import FormSend from './components/FormSend.vue';
+import FormText from './components/FormText.vue';
+import FormAgree from './components/FormAgree.vue';
+
+export default {
+    FormWrapper,
+    FormInput,
+    FormSend,
+    FormText,
+    FormAgree,
+};
